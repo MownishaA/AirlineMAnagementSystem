9324a52ceb3eb6759949a2a514f86bd543e9b69a
Mounith2005 <155057074+Mounith2005@users.noreply.github.com>
2024-10-25 12:00:41 +0530

    Update Booking.java

--- Airline management system/Home/Booking.java
+++ Airline management system/Home/Booking.java
@@ -1,1 +1,27 @@
 
+package airline;
+
+class Booking {
+    private String name;
+    private String flight;
+    private String seat;
+
+    public Booking(String name, String flight, String seat) {
+        this.name = name;
+        this.flight = flight;
+        this.seat = seat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFlight() {
+        return flight;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+}
+
